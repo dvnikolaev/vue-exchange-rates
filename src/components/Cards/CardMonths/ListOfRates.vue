cd2305f1e664178d3ea9791d8d1467ece85b42fa
dvnikolaev <dvnikolaeev@gmail.com>
2020-08-02 11:16:52 +0400

    feat: add spinner

--- src/components/Cards/CardMonths/ListOfRates.vue
+++ src/components/Cards/CardMonths/ListOfRates.vue
@@ -1,41 +1,48 @@
 <template>
   <ul class="card-month-rates">
-    <li v-for="(rate,i) in rates" :key="i" class="card-month-rates__item">
-      <span>{{ rate.name }}</span>
-      <span>{{ rateOfMonth(i) }}</span>
-    </li>
+    <div v-if="rates.length">
+      <li v-for="(rate, i) in rates" :key="i" class="card-month-rates__item">
+        <span>{{ rate.name }}</span>
+        <span>{{ rateOfMonth(i) }}</span>
+      </li>
+    </div>
+    <Spinner v-else/>
   </ul>
 </template>
 
 <script>
-import axios from 'axios';
+import { getExchangeRatesForFourMonth } from "@/services/index";
 
-import { getExchangeRatesForFourMonth } from '@/services/index';
+import Spinner from "@/components/Spinner";
 
 export default {
   props: {
     activeMonth: Number,
-    mainCurrency: String
+    mainCurrency: String,
   },
   data() {
     return {
-      rates: []
-    }
+      rates: [],
+    };
   },
   methods: {
     rateOfMonth(index) {
       return this.rates[index].values[this.activeMonth];
-    }
+    },
   },
   async created() {
     this.rates = await getExchangeRatesForFourMonth();
   },
   watch: {
     async mainCurrency(val, oldVal) {
+      this.rates = [];
       this.rates = await getExchangeRatesForFourMonth(val);
-    }
-  }
-}
+    },
+  },
+  components: {
+    Spinner,
+  },
+};
 </script>
 
 <style>
